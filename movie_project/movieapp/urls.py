bcf36ac8877b910563bb07992bf4122aa6c9e405
anjali-p-nair <100459298+anjali-p-nair@users.noreply.github.com>
2023-12-19 20:02:37 +0530

    Update urls.py

--- movie_project/movieapp/urls.py
+++ movie_project/movieapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('movie/<int:mov_id>/',views.detail,name='detail'),
     path('add/',views.add_movie,name='add_movie'),
+    path('add_new/',views.add_new,name='add_new'),
     path('update/<int:id>/',views.update,name='update'),
     path('delete/<int:id>/',views.delete,name='delete')
 ]
